259aa0e2b14bd2191bcbca3cc560ac0c5e931129
Pavlos Orfanidis <pavlos@orfanidis.xyz>
2019-08-15 15:12:21 +0300

    updated to include a website as a documentation

--- src/turnin/TurnIn.java
+++ src/turnin/TurnIn.java
@@ -163,7 +163,7 @@ public class TurnIn implements Runnable,Closeable{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         Runnable turnIn=new TurnIn(args);  
         turnIn.run();
     }
